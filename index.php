975fea15b378da470d7fde2df1b0d47cf7e6a350
Carlo Fontanos <carl.fontanos@gmail.com>
2017-02-16 23:34:14 -0600

    Moved files to github

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Facebook Preloader Style</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<br />
+	<div class="col-sm-3">
+		<div class="panel panel-default">
+			<div class="panel-heading clearfix">
+				<div class="col-xs-10">
+					<div class="animate-bg"></div>
+				</div>
+			</div>
+			<div class="panel-body">
+				<div class="col-sm-5">
+					<p class="animate-bg"><br /><br /><br /></p>
+				</div>
+				<div class="col-sm-7">
+					<p class="animate-bg"></p>
+					<p class="animate-bg col-sm-11"></p>
+					<p class="animate-bg"></p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="panel panel-default">
+			<div class="panel-heading clearfix">
+				<div class="col-xs-10">
+					<div class="animate-bg"></div>
+				</div>
+			</div>
+			<div class="panel-body">
+				<div class="col-sm-5">
+					<p class="animate-bg"><br /><br /><br /></p>
+				</div>
+				<div class="col-sm-7">
+					<p class="animate-bg"></p>
+					<p class="animate-bg col-sm-11"></p>
+					<p class="animate-bg"></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-sm-9">
+		<div class="panel panel-default">
+			<div class="panel-heading clearfix">
+				<div class="col-xs-10">
+					<div class="animate-bg"></div>
+				</div>
+			</div>
+			<div class="panel-body">
+				<div class="clearfix">
+					<div class="col-sm-3">
+						<p class="animate-bg"><br /><br /><br /><br /><br /><br /></p>
+					</div>
+					<div class="col-sm-9">
+						<p class="animate-bg col-sm-11"></p>
+						<p class="animate-bg col-sm-9"></p>
+						<p class="animate-bg col-sm-10"></p>
+						<p class="animate-bg col-sm-9"></p>
+						<p class="animate-bg"></p>
+					</div>
+				</div>
+				
+				<br />
+				
+				<div class="col-sm-6">
+					<p class="animate-bg col-sm-11"></p>
+					<p class="animate-bg col-sm-9"></p>
+					<p class="animate-bg col-sm-10"></p>
+					<p class="animate-bg col-sm-9"></p>
+					<p class="animate-bg"></p>
+				</div>
+				<div class="col-sm-6">
+					<p class="animate-bg"><br /><br /><br /><br /><br /><br /></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
